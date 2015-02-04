ecf5faa967cfd4f94ce7266ece4a93ad74fd4567
jackmaretz <giacomomaretto@gmail.com>
2015-02-04 14:46:32 +0100

    bub

---
+++ Progetto Tecnologie Web/Progetto Tecnologie Web 2.0/javascript/controllo.js
@@ -0,0 +1,42 @@
+function valida(campi)
+{
+
+	var nessuno = false;
+	for(var i=0; 1<campi.length; i++)
+	{
+		if (document.getElementById(campi[i]).value == "") 
+		{
+			nessuno = true;
+			document.getElementById(campi[i]).style.border = "2px solid red";	
+		}
+	}
+	return nessuno;
+
+}
+
+
+function controllaCampi() 
+{	
+	var errore = "";
+	var campi = ["Nome", "Cognome" , "Email" , "Messaggio"];
+
+	
+	if (valida(campi)) 
+	{
+		errore = "Alcuni campi non sono stati compilati!";
+	} 
+
+	
+	else 
+	{
+		document.forms[0].submit();
+	}	
+	
+	if (errore != "") 
+	{
+		document.getElementById("errore").innerHTML = "Errore!" +errore;
+		document.getElementById("errore").style.display = "block";
+	}
+	
+}
+
